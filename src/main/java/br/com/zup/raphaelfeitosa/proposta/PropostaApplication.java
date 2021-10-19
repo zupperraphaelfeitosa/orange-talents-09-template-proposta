0e231a1a0d14cf6e1b6ccc50aa2b15fe9ae52f0e
zupperraphaelfeitosa <raphael.feitosa@zup.com.br>
2021-10-19 15:44:17 -0300

    setup inicial

---
+++ src/main/java/br/com/zup/raphaelfeitosa/proposta/PropostaApplication.java
@@ -0,0 +1,15 @@
+package br.com.zup.raphaelfeitosa.proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/raphaelfeitosa/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.raphaelfeitosa.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
